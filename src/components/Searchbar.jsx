66661c5e0a5f9796787d27beb3649b5dc9d7c3ea
Udeh Shedrack <udehshedrackugo@gmail.com>
2022-10-20 01:48:04 +0100

    fixed bugs

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { FiSearch } from 'react-icons/fi';
 
+
 const Searchbar = () => {
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState('');
